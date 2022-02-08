bf4ea1f8ece04930e30b33b21dcb0b63ed6833ab
Pavel <serpork@ya.ru>
2022-02-08 12:51:33 +0700

    cleanup the code

---
+++ src/pages/home/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import 'pages/home/App.css';
+
+const Home = () => (
+  <div className="App">
+    <header className="App-header">1</header>
+  </div>
+);
+
+export default Home;
